2b44920e7e8a0772c45b0a1469f530f28260a8b8
alvikagal <alvikagal@gmail.com>
2020-11-18 20:30:04 +0200

    Моя админка

---
+++ admin/products.php
@@ -0,0 +1,33 @@
+<?php
+include $_SERVER["DOCUMENT_ROOT"] . "/admin/header.php"
+?>
+
+<!-- pages-title-start -->
+<section class="contact-img-area">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12 text-center">
+				<div class="con-text">
+					<h2 class="page-title">Products</h2>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+<!-- pages-title-end -->
+<section class="collapse_area coll2">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12 col-sm-12">
+				<!-- <div class="check">
+					<h2>Products</h2>
+					<!-- You Code -->
+				</div> -->
+			</div>
+		</div>	
+	</div>
+</section>
+
+<?php
+include $_SERVER["DOCUMENT_ROOT"] . "/admin/footer.php"
+?>
